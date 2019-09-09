77466969e8f6d5c61a122d96f391a25106de0721
Joshua Jonas <mr.joshuajonas@gmail.com>
2019-09-09 09:41:57 -0700

    added more questions

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -15,5 +15,37 @@ var theQuestions = [
         question: 'What is Plancks constant?',
         choices: ['6.62607004','Who is Planck', '3.1429658', '011235813'],
         correctAnswer: '6.62607004',
-    }
+    },
+
+    {
+        question: 'The beaver is the national emblem of which country?',
+        choices: ['China','Canada', 'Sweden', 'Japan'],
+        correctAnswer: 'Canada',
+    },
+
+    {
+        question: 'In which movie did Humphrey Bogart play Charlie Allnut??',
+        choices: ['LA Noire','Scream', 'Coming to America', 'African Queen'],
+        correctAnswer: 'African Queen',
+    },
+
+    {
+        question: 'What is the name of Batman’s butler?',
+        choices: ['Bob', 'Harvey', 'Alfred', 'Joker'],
+        correctAnswer: 'Alfred',
+    },
+
+    {
+        question: 'In which city is Jim Morrison buried??',
+        choices: ['NYC', 'Paris', 'London', 'Beijing'],
+        correctAnswer: 'Paris',
+    },
+
+    {
+        question: 'In Greek mythology, how many graces are there?',
+        choices: ['3', '2', '1', '5'],
+        correctAnswer: '3',
+    },
+
+    
 ];
